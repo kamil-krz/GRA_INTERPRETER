6e56a50d43e6e7150f64d79e31a40207b56d2332
Kamil <k-krzyzewski@o2.pl>
2017-05-25 18:55:47 +0200

    mapa + pociski + jazda

--- Klasy.py
+++ Klasy.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import (QRectF, Qt)
 from PyQt5.QtGui import (QBrush, QColor, QImage)
 from PyQt5.QtWidgets import QGraphicsItem
+from PyQt5 import QtTest
 
 
 class czolg(QGraphicsItem):
@@ -10,6 +11,7 @@ class czolg(QGraphicsItem):
         self.icon = icon
         self.xy = xy
         self.dir = dir
+        #self.kafelki = {}
 
     def boundingRect(self):
         return QRectF(0, 0, 500, 500)
@@ -31,8 +33,135 @@ class czolg(QGraphicsItem):
         if self.dir == 270:
             painter.drawRect(x * 25 + 10, y * 25 + 10, 5, 15)
 
-
-
-    def goto(self, target):
-        self.xy = target
+    def obrot_prawo(self):
+        self.dir = self.dir - 90
+        if self.dir <0:
+            self.dir = self.dir +360
         self.update()
+        QtTest.QTest.qWait(500)
+
+    def obrot_lewo(self):
+        self.dir = self.dir + 90
+        if self.dir >=360:
+            self.dir = self.dir - 360
+        self.update()
+        QtTest.QTest.qWait(500)
+
+    def jedz_prosto(self, dist=1):
+        for i in range(dist):
+            if self.dir == 0:
+                xy = (self.xy[0] + 1, self.xy[1])
+            if self.dir == 180:
+                xy = (self.xy[0] - 1, self.xy[1])
+            if self.dir == 90:
+                xy = (self.xy[0],self.xy[1] - 1)
+            if self.dir == 270:
+                xy = (self.xy[0],self.xy[1] + 1)
+            if self.kafelki[xy].typ == 'chodnik':
+                self.xy = xy
+            self.update()
+            QtTest.QTest.qWait(500)
+
+    def jedz_tyl(self, dist=1):
+        for i in range(dist):
+            if self.dir == 0:
+                xy = (self.xy[0] - 1, self.xy[1])
+            if self.dir == 180:
+                xy = (self.xy[0] + 1, self.xy[1])
+            if self.dir == 90:
+                xy = (self.xy[0],self.xy[1] + 1)
+            if self.dir == 270:
+                xy = (self.xy[0],self.xy[1] - 1)
+            if self.kafelki[xy].typ == 'chodnik':
+                self.xy = xy
+            self.update()
+            QtTest.QTest.qWait(500)
+
+    def strzal(self):
+        if self.dir == 0:
+            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+        if self.dir == 180:
+            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+        if self.dir == 90:
+            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+        if self.dir == 270:
+            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+
+        p = pocisk(xy=xy,dir = self.dir)
+        self.scene.addItem(p)
+        p.lot()
+
+
+
+
+    def run(self, kafelki, scene):
+        self.kafelki = kafelki
+        self.scene = scene
+##########################################################################
+##########################################################################
+        self.strzal()
+        self.jedz_prosto(3)
+        self.obrot_prawo()
+        self.strzal()
+
+
+
+
+
+class kafelek(QGraphicsItem):
+    def __init__(self, xy=(-1, -1), typ = 'chodnik'):
+        QGraphicsItem.__init__(self)
+        self.typ = typ
+        if self.typ == 'chodnik':
+            self.colour = 'white'
+        elif self.typ == 'sciana_zn':
+            self.colour = 'gray'
+        elif self.typ == 'sciana_nzn':
+            self.colour = 'black'
+        self.xy = xy
+
+    def boundingRect(self):
+        return QRectF(0, 0, 500, 500)
+
+    def paint(self, painter, option, widget):
+        x, y = self.xy
+        colour = QBrush(QColor(self.colour))
+        painter.setBrush(colour)
+        painter.drawRect(x * 25 , y * 25 , 25, 25)
+
+
+class pocisk(QGraphicsItem):
+    def __init__(self, xy=(-1, -1), dir = 0):
+        QGraphicsItem.__init__(self)
+        self.xy = xy
+        self.dir = dir
+
+
+    def boundingRect(self):
+        return QRectF(0, 0, 500, 500)
+
+    def paint(self, painter, option, widget):
+        x, y = self.xy
+        colour = QBrush(QColor('yellow'))
+        painter.setBrush(colour)
+        painter.drawRect(x-1, y-1, 3, 3)
+
+    def lot(self):
+        if self.dir == 0:
+            xy = (self.xy[0] + 1, self.xy[1])
+        if self.dir == 180:
+            xy = (self.xy[0] - 1, self.xy[1])
+        if self.dir == 90:
+            xy = (self.xy[0], self.xy[1] - 1)
+        if self.dir == 270:
+            xy = (self.xy[0], self.xy[1] + 1)
+        self.xy = xy
+        self.update()
+
+
+
+
+
+
+
+
--- main_nasz.py
+++ main_nasz.py
@@ -1,7 +1,9 @@
-import sys
+import sys, psutil, os
 from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtCore import QRectF, QTimer, Qt
 from PyQt5.QtWidgets import *
 from Klasy import *
+
 
 from gui_nasze import Ui_Form
 
@@ -11,6 +13,7 @@ def exception_hook(exctype, value, traceback):
     sys.exit(1)
 sys.excepthook = exception_hook
 
+plansza_id=1
 
 class MyForm(QMainWindow, Ui_Form):
 
@@ -20,16 +23,45 @@ class MyForm(QMainWindow, Ui_Form):
         self.setupUi(self)
 
         self.scene = QGraphicsScene()
-        self.czolgi = czolg(xy=(10, 10))
+        self.czolgi = czolg(xy=(12, 12))
+        self.pociski = {}
+        self.kafelki = {}
+        file = open('plansza'+str(plansza_id)+'.txt')
+
+        for i, line in enumerate(file):
+            for j in range(0,20):
+                if line[j] == ' ':
+                    self.kafelki[(j,i)] = kafelek(xy=(j,i),typ = 'chodnik')
+                if line[j] == '#':
+                    self.kafelki[(j,i)] = kafelek(xy=(j,i), typ='sciana_nzn')
+                if line[j] == '*':
+                    self.kafelki[(j,i)] = kafelek(xy=(j,i), typ='sciana_zn')
+
+        self.timer = QTimer(self)
 
         self.plansza.setScene(self.scene)
+
+        for xy in self.kafelki.keys():
+                self.scene.addItem(self.kafelki[xy])
         self.scene.addItem(self.czolgi)
 
+
+        self.timer.timeout.connect(self.latanie)
         self.b_start.clicked.connect(self.start)
 
 
     def start(self):
-        self.czolgi.goto((12,12))
+        self.timer.start(10)
+        self.czolgi.run(self.kafelki, self.scene)
+
+    def latanie(self):
+        for i in self.scene.items():
+            if type(i) == pocisk:
+                i.lot()
+
+    # def closeEvent(self, event):
+    #     pass
+
 
 
 
@@ -38,4 +70,6 @@ if __name__ == "__main__":
     window = MyForm()
     window.show()
     sys.exit(app.exec_())
-    sys.exit(app.exec_())
+
+
+
