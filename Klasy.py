b43332f440a4b33fdf5ba20fb9f912b33121337d
gree246 <g.derda2@gmail.com>
2017-06-20 17:44:00 +0200

    wątki

--- Klasy.py
+++ Klasy.py
@@ -7,24 +7,30 @@ import sys
 from sys import exc_info
 import traceback
 import time
+import random
 
 
 
 class czolg(QGraphicsItem):
-    def __init__(self, xy=(-1, -1), dir=180, obrazki = None):
+    def __init__(self, xy=(-1, -1), dir=180, obrazki = None,type='stojacy',delay=500,scene=None,hp=1):
         QGraphicsItem.__init__(self)
+        self.scene = scene
+        self.delay=delay
         self.xy = xy
         self.dir = dir
         self.obrazek = obrazki['czolg']
+        self.type=type
+        self.hp = hp
+
 
 
     def boundingRect(self):
         return QRectF(0, 0, 500, 500)
 
-
-    def paint(self, painter,option, widget):
+    def paint(self, painter, option, widget):
         x, y = self.xy
         rect = QRectF(x * 25, y * 25, 25, 25)
+
         if self.dir == 0:
             obrot = QTransform().rotate(90)
             painter.drawImage(rect, self.obrazek.transformed(obrot))
@@ -38,72 +44,109 @@ class czolg(QGraphicsItem):
             painter.drawImage(rect, self.obrazek.transformed(obrot))
 
     def obrot_prawo(self):
+        self.e.wait()
         self.dir = self.dir - 90
         if self.dir <0:
             self.dir = self.dir +360
-        self.update()
-        QtTest.QTest.qWait(500)
+        self.e.clear()
 
     def obrot_lewo(self):
+        self.e.wait()
         self.dir = self.dir + 90
         if self.dir >=360:
             self.dir = self.dir - 360
-        self.update()
-        QtTest.QTest.qWait(500)
+        self.e.clear()
 
-    def jedz_prosto(self, dist=1):
-        for i in range(dist):
-            if self.dir == 0:
-                xy = (self.xy[0] + 1, self.xy[1])
-            if self.dir == 180:
-                xy = (self.xy[0] - 1, self.xy[1])
-            if self.dir == 90:
-                xy = (self.xy[0],self.xy[1] - 1)
-            if self.dir == 270:
-                xy = (self.xy[0],self.xy[1] + 1)
+    def jedz(self, dist=1):
+        for i in range(abs(dist)):
+            self.e.wait()
+            if dist>0:
 
-            for i in self.scene.items():
-                if type(i) == czolg and i.xy == xy:
-                    xy = self.xy
+                if self.dir == 0:
+                    xy = (self.xy[0] + 1, self.xy[1])
+                if self.dir == 180:
+                    xy = (self.xy[0] - 1, self.xy[1])
+                if self.dir == 90:
+                    xy = (self.xy[0],self.xy[1] - 1)
+                if self.dir == 270:
+                    xy = (self.xy[0],self.xy[1] + 1)
 
-            if self.kafelki[xy].typ == 'chodnik':
-                self.xy = xy
-            self.update()
-            QtTest.QTest.qWait(500)
 
-    def jedz_tyl(self, dist=1):
-        for i in range(dist):
-            if self.dir == 0:
-                xy = (self.xy[0] - 1, self.xy[1])
-            if self.dir == 180:
-                xy = (self.xy[0] + 1, self.xy[1])
-            if self.dir == 90:
-                xy = (self.xy[0],self.xy[1] + 1)
-            if self.dir == 270:
-                xy = (self.xy[0],self.xy[1] - 1)
+            elif dist<0:
+
+                if self.dir == 0:
+                    xy = (self.xy[0] - 1, self.xy[1])
+                if self.dir == 180:
+                    xy = (self.xy[0] + 1, self.xy[1])
+                if self.dir == 90:
+                    xy = (self.xy[0], self.xy[1] + 1)
+                if self.dir == 270:
+                    xy = (self.xy[0], self.xy[1] - 1)
+
 
             for i in self.scene.items():
-                if type(i) == czolg and i.xy == xy:
-                    xy = self.xy
+                if  i.xy == xy:
+                    if (type(i) == czolg or type(i) == player)and i!=self:
+                        xy = self.xy
+                        break
+                    elif type(i)==kafelek and i.typ!='chodnik':
+                        xy = self.xy
+                        break
 
-            if self.kafelki[xy].typ == 'chodnik':
-                self.xy = xy
-            self.update()
-            QtTest.QTest.qWait(500)
+            self.xy = xy
+            self.e.clear()
+
+
+
 
     def strzal(self):
+        self.e.wait()
         if self.dir == 0:
-            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+            xy = (self.xy[0]*25+25, self.xy[1]*25+12)
         if self.dir == 180:
-            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+            xy = (self.xy[0]*25, self.xy[1]*25+12)
         if self.dir == 90:
-            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+            xy = (self.xy[0]*25+12, self.xy[1]*25)
         if self.dir == 270:
-            xy = (self.xy[0]*25+12, self.xy[1]*25+12)
+            xy = (self.xy[0]*25+12, self.xy[1]*25+25)
 
-        p = pocisk(xy=xy,dir = self.dir, scena = self.scene, kafelki = self.kafelki)
+        p=pocisk(xy=xy,dir = self.dir, scena = self.scene)
         self.scene.addItem(p)
-        QtTest.QTest.qWait(500)
+
+        self.e.clear()
+
+
+    def go_AI(self, e):
+        self.e=e
+        if self.type=='stojacy':
+            return
+        elif self.type=='random':
+            while(True):
+                if(self.hp<=0):
+                    return 0
+                move=random.randint(1, 4)
+                if move==1:
+                    self.jedz()
+                elif move==2:
+                    self.obrot_lewo()
+                elif move==3:
+                    self.obrot_prawo()
+                elif move==4:
+                    self.strzal()
+        elif self.type=='prosto':
+            while(True):
+                if (self.hp <= 0):
+                    return 0
+                self.jedz()
+                self.strzal()
+
+
+
+    def hit(self):
+        self.hp-=1
+        if self.hp<=0:
+            self.scene.removeItem(self)
+
 
 
 class kafelek(QGraphicsItem):
@@ -119,6 +162,7 @@ class kafelek(QGraphicsItem):
     def paint(self, painter, option, widget):
         x, y = self.xy
         rect = QRectF(x * 25, y * 25, 25, 25)
+
         if self.typ == 'chodnik':
             colour = QBrush(QColor('black'))
             painter.setBrush(colour)
@@ -131,76 +175,108 @@ class kafelek(QGraphicsItem):
 
 
 class pocisk(QGraphicsItem):
-    def __init__(self, xy=(-1, -1), dir = 0, scena = {} , kafelki = {}):
+    def __init__(self, xy=(-1, -1), dir = 0, scena = {} ):
         QGraphicsItem.__init__(self)
         self.xy = xy
         self.dir = dir
         self.scene = scena
-        self.kafelki = kafelki
-
 
     def boundingRect(self):
         return QRectF(0, 0, 500, 500)
 
     def paint(self, painter, option, widget):
         x, y = self.xy
+
         colour = QBrush(QColor('yellow'))
         painter.setBrush(colour)
         painter.drawRect(x-1, y-1, 3, 3)
 
     def lot(self):
-
         if self.dir == 0:
-            xy = (self.xy[0] + 1, self.xy[1])
+            xy = (self.xy[0] + 2, self.xy[1])
         elif self.dir == 180:
-            xy = (self.xy[0] - 1, self.xy[1])
+            xy = (self.xy[0] - 2, self.xy[1])
         elif self.dir == 90:
-            xy = (self.xy[0], self.xy[1] - 1)
+            xy = (self.xy[0], self.xy[1] - 2)
         elif self.dir == 270:
-            xy = (self.xy[0], self.xy[1] + 1)
-
-        if self.kafelki[((int(xy[0]/25)),int(xy[1]/25))].typ == 'chodnik':
-            self.xy = xy
-        elif self.kafelki[((int(xy[0]/25)),int(xy[1]/25))].typ == 'sciana_zn':
-            self.scene.removeItem(self)
-            self.kafelki[((int(xy[0] / 25)), int(xy[1] / 25))].typ = 'chodnik'
-        elif self.kafelki[((int(xy[0]/25)),int(xy[1]/25))].typ == 'sciana_nzn':
-            self.scene.removeItem(self)
-
+            xy = (self.xy[0], self.xy[1] + 2)
+        else:
+            xy=self.xy
         for i in self.scene.items():
-            if type(i) == czolg and i.xy == (((int(xy[0]/25)),int(xy[1]/25))):
-                self.scene.removeItem(i)
-                self.scene.removeItem(self)
-        self.update()
+            if i.xy==((int(xy[0]/25)),int(xy[1]/25)):
+                if type(i)==kafelek and i.typ=='chodnik':
+                    self.xy = xy
+
+                elif type(i)==kafelek and i.typ=='sciana_zn':
+                    self.scene.removeItem(self)
+                    i.typ = 'chodnik'
+                    break
+                elif type(i)==kafelek and i.typ=='sciana_nzn':
+                    self.scene.removeItem(self)
+                    break
+
+                elif type(i) == czolg:
+                    i.hit();
+                    self.scene.removeItem(self)
+                    break
+                elif type(i)==player:
+                    self.scene.removeItem(self)
+                    i.hit();
+                    break
+
 
 class player(czolg):
-    def __init__(self, xy=(-1, -1), dir=180, obrazki = None):
+    def __init__(self, xy=(-1, -1), dir=180, obrazki = None,delay=700,scene=None):
         QGraphicsItem.__init__(self)
         self.xy = xy
         self.dir = dir
         self.obrazek = obrazki['player']
-
-
-    def run(self, kafelki, scene, kod):
-        self.kafelki = kafelki
+        self.delay=delay
+        self.hp=3
         self.scene = scene
-        strzal = self.strzal
-        jedz_tyl = self.jedz_tyl
-        jedz_prosto = self.jedz_prosto
-        obrot_prawo = self.obrot_prawo
-        obrot_lewo = self.obrot_lewo
+
+
+    def run(self, kod,e,e_krokowa):
+        self.e=e
+        self.e_krokowa=e_krokowa
+        kod2=''
+        self.licznik=0;
+
+        for idx,l in enumerate(kod.splitlines()):
+            wciecia=''
+            for a in l:
+                if a==' ' or a=='\t':
+                    wciecia+=a
+                else:
+                    if a.isalnum() or a=='_' or  a in '()-._,<>[]{};':
+                        kod2 += wciecia + 'self.licznik=' + str(idx) + '\n'
+                        kod2 += wciecia + 'self.e_krokowa.wait()\n' + wciecia + 'self.e_krokowa.clear()' + '\n'
+                        kod2 += l + '\n'
+                    break
+            if wciecia==l:
+                kod2 += ' \n \n \n \n'
+
+
+        print(kod2)
+
+        # czolg.strzal = self.strzal
+        #
+        # czolg.jedz = self.jedz
+        # czolg.obrot_prawo = self.obrot_prawo
+        # czolg.obrot_lewo = self.obrot_lewo
         ##########################################################################
         ##########################################################################
-        if 'class' in kod:
+        if 'class' in kod2:
             return ['Nie wolno definować nowych klas']
-        elif 'import' in kod:
+        elif 'import' in kod2:
             return ['Nie wolno importować']
         try:
-            exec(kod)
+            exec(kod2)
             return ['Wykonano kod']
         except:
             formatted_lines = traceback.format_exc().splitlines()
             formatted_lines[3]=  formatted_lines[3].split(',')[1].lstrip() + ":"
+            print (formatted_lines[3:])
             return formatted_lines[3:]
             # except Exception as err:
             #     print(type(err))
--- main_nasz.py
+++ main_nasz.py
@@ -1,12 +1,14 @@
-import sys, psutil, os
+import threading
+
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtGui import QImage
 from PyQt5.QtCore import QRectF, QTimer, Qt
 from PyQt5.QtWidgets import *
 from Klasy import *
-
-
+from ThreadWithExc import *
 from gui_nasze import Ui_Form
+
+
 
 sys._excepthook = sys.excepthook
 def exception_hook(exctype, value, traceback):
@@ -14,7 +16,6 @@ def exception_hook(exctype, value, traceback):
     sys.exit(1)
 sys.excepthook = exception_hook
 
-plansza_id=1
 
 class MyForm(QMainWindow, Ui_Form):
 
@@ -24,67 +25,145 @@ class MyForm(QMainWindow, Ui_Form):
         self.setupUi(self)
         self.obrazki = {'player': QImage("graphics/player.jpg"), 'czolg': QImage("graphics/czolg.jpg"), 'sciana_zniszczalna': QImage("graphics/sciana_zniszczalna.jpg"), 'sciana_niezniszczalna': QImage("graphics/sciana_niezniszczalna.jpg"), }
         self.scene = QGraphicsScene()
-        self.czolgi = []
-        self.player = None
-        self.pociski = {}
-        self.kafelki = {}
-        file = open('plansza'+str(plansza_id)+'.txt')
-
-        for i, line in enumerate(file):
-            for j in range(0,20):
-                if line[j] == ' ':
-                    self.kafelki[(j,i)] = kafelek(xy=(j,i),typ = 'chodnik',obrazki = self.obrazki)
-                if line[j] == '#':
-                    self.kafelki[(j,i)] = kafelek(xy=(j,i), typ='sciana_nzn',obrazki = self.obrazki)
-                if line[j] == '*':
-                    self.kafelki[(j,i)] = kafelek(xy=(j,i), typ='sciana_zn',obrazki = self.obrazki)
-                if line[j] == 'P':
-                    self.player = player(xy=(j, i), obrazki = self.obrazki)
-                    self.kafelki[(j, i)] = kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki)
-                if line[j] == '<':
-                    self.czolgi.append(czolg(xy=(j, i), dir = 180, obrazki = self.obrazki))
-                    self.kafelki[(j, i)] = kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki)
-                if line[j] == '>':
-                    self.czolgi.append(czolg(xy=(j, i), dir=0, obrazki=self.obrazki))
-                    self.kafelki[(j, i)] = kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki)
-                if line[j] == '^':
-                    self.czolgi.append(czolg(xy=(j, i), dir=90, obrazki=self.obrazki))
-                    self.kafelki[(j, i)] = kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki)
-                if line[j] == 'v':
-                    self.czolgi.append(czolg(xy=(j, i), dir=270, obrazki=self.obrazki))
-                    self.kafelki[(j, i)] = kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki)
+        self.scene.setItemIndexMethod(QGraphicsScene.NoIndex)
 
 
-
+        self.MPM=120        #moves per minute
+        self.threads_list = []
+        self.player=None
+        self.player_thread=None
 
         self.timer = QTimer(self)
-
+        self.timer_action = QTimer(self)
         self.plansza.setScene(self.scene)
-
-        for xy in self.kafelki.keys():
-                self.scene.addItem(self.kafelki[xy])
-        self.scene.addItem(self.player)
-        for i in self.czolgi:
-            self.scene.addItem(i)
-
+        self.ActionEvent = threading.Event()
+        self.krokowaEvent = threading.Event()
 
         self.timer.timeout.connect(self.latanie)
+        self.timer_action.timeout.connect(self.action)
         self.b_start.clicked.connect(self.start)
         self.b_reset.clicked.connect(self.reset)
 
+        self.laduj_plansze('plansza1.txt')
+
+    def laduj_plansze(self,nazwa):
+        file = open(nazwa)
+        for i, line in enumerate(file):
+            for j in range(0, 20):
+                if line[j] == ' ':
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+                if line[j] == '#':
+                    self.scene.addItem(kafelek(xy=(j, i), typ='sciana_nzn', obrazki=self.obrazki))
+                if line[j] == '*':
+                    self.scene.addItem(kafelek(xy=(j, i), typ='sciana_zn', obrazki=self.obrazki))
+                if line[j] == 'P':
+                    self.player=player(xy=(j, i), obrazki=self.obrazki, scene=self.scene)
+                    self.scene.addItem(self.player)
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+                if line[j] == '<':
+                    self.scene.addItem(czolg(xy=(j, i), dir=180, obrazki=self.obrazki, type='prosto', scene=self.scene))
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+                if line[j] == '>':
+                    self.scene.addItem(czolg(xy=(j, i), dir=0, obrazki=self.obrazki, type='prosto', scene=self.scene))
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+                if line[j] == '^':
+                    self.scene.addItem(czolg(xy=(j, i), dir=90, obrazki=self.obrazki, type='random', scene=self.scene))
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+                if line[j] == 'v':
+                    self.scene.addItem(czolg(xy=(j, i), dir=270, obrazki=self.obrazki, type='prosto', scene=self.scene))
+                    self.scene.addItem(kafelek(xy=(j, i), typ='chodnik', obrazki=self.obrazki))
+
+        for i in self.scene.items():
+            if type(i) != kafelek:
+                i.setZValue(0.5)
+
+
+
+    def action(self):
+        self.ActionEvent.set()
+
 
     def start(self):
-        self.timer.start(10)
+        self.timer.start(int(60*1000/self.MPM/20))
+        self.timer_action.start(int(60*1000/self.MPM))
+
         kod = self.textBox.toPlainText()
-        for i in self.player.run(self.kafelki, self.scene, kod):
-            print(i)
+        for idx,item in enumerate(self.scene.items()):
+            if type(item)==player:
+                self.player_thread = ThreadWithExc(name='player_thread',
+                     target=item.run,
+                     args=(kod,self.ActionEvent,self.krokowaEvent,))
+
+
+                self.threads_list.append(self.player_thread)
+                # for i in self.player.run( kod):
+                #     print(i)
+            elif type(item)==czolg:
+                t = ThreadWithExc(name='czolg_'+str(idx),
+                                     target=item.go_AI,
+                                     args=(self.ActionEvent,))
+                self.threads_list.append(t)
+
+
+
+        for t in self.threads_list:
+            t.daemon = True
+            t.start()
+
+
     def reset(self):
-        self.reset()
+
+        self.krokowaEvent.set()
+
+
+
+        # DZIALAJACY RESET
+        # for t in self.threads_list:
+        #     if t.isAlive():
+        #         t.raiseExc(Exception)
+        #         time.sleep(0.1)
+        # self.threads_list=[]
+        # self.timer.stop()
+        # self.timer_action.stop()
+        # self.scene.clear()
+        # self.laduj_plansze('plansza1.txt')
+
+
+
+
+        # for t in self.threads_list:
+        #     if t.isAlive():
+        #         t.raiseExc(Exception)
+        #         time.sleep(0.1)
+        # self.threads_list=[]
+        # self.timer.stop()
+        # self.timer_action.stop()
+
+
+
+        # self.scene.addItem(kafelek())
+        pass
 
     def latanie(self):
         for i in self.scene.items():
             if type(i) == pocisk:
                 i.lot()
+            elif type(i)==player:
+                print(i.licznik)
+                if i.hp<=0:
+                    for t in self.threads_list:
+                        if t.getName()=='player_thread':
+                            if t.isAlive():
+                                t.raiseExc(Exception)
+                                time.sleep(0.1)
+
+        # self.scene.addItem(pocisk())
+        self.scene.update()
+
+
+
+
+
 
     # def closeEvent(self, event):
     #     pass
